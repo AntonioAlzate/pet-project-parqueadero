543d97c36e0a30b08217772b1b09ced6b9741fe1
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-29 15:26:19 -0500

    Commit inicial.
    Se crean los proyectos Back y Front vacíos.

---
+++ back-parqueadero/src/main/java/com/co/sofka/project/parqueadero/ParqueaderoApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.project.parqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoApplication.class, args);
+	}
+
+}
---
+++ back-parqueadero/src/test/java/com/co/sofka/project/parqueadero/ParqueaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.project.parqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
